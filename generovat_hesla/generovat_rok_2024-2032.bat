d887f37246cac179d9d1eb4d0ff9ebc51fd9aa05
Martin pávek <martin_pavek@email.cz>
2025-02-20 10:01:25 +0100

    Update generovat_rok_2024-2032.bat

--- generovat_hesla/generovat_rok_2024-2032.bat
+++ generovat_hesla/generovat_rok_2024-2032.bat
@@ -1,9 +1,9 @@
 @echo off
-title rok 2024 - 2032
-echo budou vygenerovany hesla pro rok 2024-2032
+title rok 2025 - 2032
+echo budou vygenerovany hesla pro rok 2025-2032
 pause
 
-for /L %%R IN (24,1,32) DO (
+for /L %%R IN (25,1,32) DO (
 REM           od,krok,do
 echo generuju hesla pro rok 20%%R
 lua52.exe generuj_hesla_pro_verzi_6.lua 20%%R
